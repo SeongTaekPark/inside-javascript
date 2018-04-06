9cd3ef3d55dc7bb8b10274ffd5fca87c002938d2
Seongtaek Park <esulove81@gmail.com>
2018-04-06 11:13:34 +0900

    5.4.2.2 함수의 캡슐화

---
+++ src/5.12.js
@@ -0,0 +1,24 @@
+// 5.4.2.2 함수의 캡슐화
+
+var getCompletedStr = (function() {
+  var buffAr = [
+    'I am ',
+    '',
+    '. I live in ',
+    '',
+    '. I\'m ',
+    '',
+    ' years old.'
+  ];
+
+  return (function(name, city, age) {
+    buffAr[1] = name;
+    buffAr[3] = city;
+    buffAr[5] = age;
+
+    return buffAr.join('');
+  });
+})();
+
+var str = getCompletedStr('taek', 'seoul', 38);
+console.log(str);
