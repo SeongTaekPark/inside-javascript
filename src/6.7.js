f18b397a677351c0ab52e8128f4d81e134eac771
Seongtaek Park <esulove81@gmail.com>
2018-04-10 10:52:04 +0900

    6.2.2 클래스 기반의 상속 -> 부모 클래스의 생성자를 호출

--- src/6.7.js
+++ src/6.7.js
@@ -14,7 +14,13 @@ Person.prototype.getName = function() {
   return this.name;
 }
 
+// 이렇게 하면 new Student("zzoon") 으로 me 인스턴스를 생성할 때 문제가 있음.
+// function Student(arg) {
+// }
+
 function Student(arg) {
+  // 이렇게 부모 클래스의 생성자를 호출해야 한다.
+  Person.apply(this, arguments);
 }
 
 var you = new Person("iamstpark");
